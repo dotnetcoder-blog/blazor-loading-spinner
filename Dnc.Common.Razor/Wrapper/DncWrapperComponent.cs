533c87db5784e59683a2b816986a2499b5c69eed
Alaaddin <alaaiddin.albakkour@gmail.com>
2024-05-10 00:00:32 +0200

    Add project files.

---
+++ Dnc.Common.Razor/Wrapper/DncWrapperComponent.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Dnc.Common.Razor.Wrapper
+{
+    public class DncWrapperComponent:ComponentBase
+    {
+        [Parameter] 
+        public RenderFragment ChildContent { get; set; }
+        public bool Loading { get; private set; }
+
+        private CancellationTokenSource cancellationTokenSource;
+        public async Task<T> AwaitTask<T>(Task<T> task)
+        {
+            SetLoading(true);
+
+            try
+            {
+                cancellationTokenSource = new CancellationTokenSource();
+                return await task.WaitAsync(cancellationTokenSource.Token);
+            }
+            catch (TaskCanceledException e)
+            {
+                e.Data.Add("CanceledTask", "true");
+                throw;
+            }
+            finally
+            {
+                SetLoading(false);
+                cancellationTokenSource = null;
+            }
+        }
+        public void SetLoading(bool loadoing)
+        {
+            Loading = loadoing;
+            StateHasChanged();
+        }
+    }
+}
---
+++ Dnc.Loading.WebApp/Components/Pages/EmployeesComponent.cs
@@ -0,0 +1,36 @@
+﻿using Dnc.Common.Razor.Wrapper;
+using Microsoft.AspNetCore.Components;
+using System.Text.Json;
+
+namespace Dnc.Loading.WebApp.Components.Pages
+{
+    public class EmployeesComponent : ComponentBase
+    {
+        [CascadingParameter] protected 
+        DncWrapper DncWrapper { get; set; }
+
+
+        protected List<Employee> Employees = null;
+        protected override async Task OnInitializedAsync()
+        {
+            Employees = await DncWrapper.AwaitTask(GetEmployees());
+        }
+
+        private async Task<List<Employee>> GetEmployees()
+        {
+            await Task.Delay(7000);
+            var json = File.ReadAllText(@"./Data/employees.json");
+            return JsonSerializer.Deserialize<List<Employee>>(json, options: new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+    }
+}
